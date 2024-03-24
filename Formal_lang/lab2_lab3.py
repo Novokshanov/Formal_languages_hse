1676dab529490a890e1e8ca7ef37bcd49d1e2da1
Novokshanov <Arxiv300800@gmail.com>
2024-03-24 19:04:31 +0300

    Initial commit

---
+++ Formal_lang/lab2_lab3.py
@@ -0,0 +1,148 @@
+try:
+    from nltk import CFG, Production, Nonterminal
+    from hazm import word_tokenize, POSTagger
+    from nltk import ChartParser
+    from langdetect import detect
+    import matplotlib.pyplot as plt
+    tagger = POSTagger(model='pos_tagger.model')
+except:
+    print("Make sure the following modules are installed: nltk, hazm, matplotlib.pyplot, langdetect. There also should be a 'pos_tagger.model' file in the directory that can be downloaded via the link: https://drive.google.com/file/d/1Q3JK4NVUC2t5QT63aDiVrCRBV225E_B3")
+
+base_grammar = CFG.fromstring("""
+    FulS -> S PUNCT | S | PUNCT S | PUNCT S PUNCT
+    S -> NP VP | VP | VP CP | VP SConjP
+    SConjP -> SCONJ VP
+    CP -> CONJ NP | CONJ VP
+    CompV -> N V | ADJ V
+    NP_ez -> N_ez NP | N_ez AdjP | N_ez NP_ez | NP_ez PRO
+    ProP -> PRO NP | PRO NP_ez
+    NP -> N | N_ez | N NP | N AdjP | NP CONJ NP | N AdjP | NP N | NP PRO | PRO NP | NUM NP | ProP CONJ NP | NP CONJ ProP | PUNCT N PUNCT | PUNCT NP PUNCT
+    VP -> NP V | NP_ez V | AdjP V | ADV VP | N VP | PP V | PP VP | PostP V | ProP VP | V | V VP | DetP VP
+    ProP -> PRO | PRO CONJ PRO
+    DemP -> DEM NP
+    AdjP -> ADJ | ADJ_ez | ADJ_ez AdjP | ADJ_ez DetP | ADJ_ez NP | ADJ_ez ProP
+    DetP -> DET NP | DetP ADJ
+    PP -> P NP | P NP_ez | P DetP | P CONJ | PP VP | NUM PP | P NUM
+    PostP -> NP P | ProP P
+""")
+        
+def parse_sentence(sentence:str):
+    try:
+        # Проверяем язык текста
+        lang = detect(sentence)
+        if lang != 'fa':
+            return print("Пожалуйста, введите предложение на персидском языке.")
+        else:
+            tagged_s = tagger.tag(word_tokenize(sentence))
+        
+        return tagged_s
+    except:
+        return print("Не удалось распознать предложение.")
+    
+def update_grammar_with_pos_tags(base_grammar, tagged_s):
+    # Собираем множества слов для каждой части речи
+    nouns = set()
+    nouns_ez = set()
+    pronouns = set()
+    verbs = set()
+    adjectives = set()
+    adjectives_ez = set()
+    adverbs = set()
+    demonstratives = set()
+    determinatives = set()
+    prepositions = set()
+    conjunctions = set()
+    sconjunctions = set()
+    punctuation = set()
+    numer = set()
+    for word, tag in tagged_s:
+        if tag == 'NOUN':
+            nouns.add(word)
+        elif tag == 'NOUN,EZ':
+            nouns_ez.add(word)
+        elif tag.startswith('V'):
+            verbs.add(word)
+        elif tag == 'ADJ':
+            adjectives.add(word)
+        elif tag == 'ADJ,EZ':
+            adjectives_ez.add(word)
+        elif tag == 'DEM':
+            demonstratives.add(word)
+        elif tag == 'DET':
+            determinatives.add(word)
+        elif tag == 'ADP' or tag == 'ADP,EZ':
+            prepositions.add(word)
+        elif tag == 'CCONJ':
+            conjunctions.add(word)
+        elif tag == 'SCONJ':
+            sconjunctions.add(word)
+        elif tag == 'PRON':
+            pronouns.add(word)
+        elif tag == 'NUM':
+            numer.add(word)
+        elif tag == 'ADV':
+            adverbs.add(word)
+        elif tag == 'PUNCT':
+            punctuation.add(word)
+
+    # Создаем список правил для обновления грамматики
+    new_productions = []
+    for noun in nouns:
+        new_productions.append(Production('N', [noun]))
+    for noun_ez in nouns_ez:
+        new_productions.append(Production('N_ez', [noun_ez]))
+    for pronoun in pronouns:
+        new_productions.append(Production('PRO', [pronoun]))
+    for verb in verbs:
+        new_productions.append(Production('V', [verb]))
+    for adj in adjectives:
+        new_productions.append(Production('ADJ', [adj]))
+    for adj_ez in adjectives_ez:
+        new_productions.append(Production('ADJ_ez', [adj_ez]))
+    for num in numer:
+        new_productions.append(Production('NUM', [num]))
+    for adv in adverbs:
+        new_productions.append(Production('ADV', [adv]))
+    for conj in conjunctions:
+        new_productions.append(Production('CONJ', [conj]))
+    for sconj in sconjunctions:
+        new_productions.append(Production('SCONJ', [sconj]))
+    for dem in demonstratives:
+        new_productions.append(Production('DEM', [dem]))
+    for det in determinatives:
+        new_productions.append(Production('DET', [det]))
+    for prep in prepositions:
+        new_productions.append(Production('P', [prep]))
+    for punct in punctuation:
+        new_productions.append(Production('PUNCT', [punct]))
+    
+    # Создаем CFG с объединенными правилами
+    updated_grammar = CFG(base_grammar.start(), base_grammar.productions() + new_productions)
+    
+    words_and_phrases = {}
+    # Избавляемся от ненужных одиночных кавычек в правилах
+    for production in updated_grammar.productions():
+        nonterminal = production.lhs()  
+        if isinstance(production.rhs()[0], str):
+            if nonterminal not in words_and_phrases:
+                words_and_phrases[nonterminal] = [] 
+            words_and_phrases[nonterminal].append(production.rhs()[0])
+
+    productions = list(base_grammar.productions())
+    for nonterminal, words_list in words_and_phrases.items():
+        for word in words_list:
+            productions.append(Production(Nonterminal(nonterminal), [word]))
+
+    grammar = CFG(base_grammar.start(), productions)
+    return grammar
+
+def get_the_tree(sentence:str):
+    tagged_s = parse_sentence(sentence)
+    grammar = update_grammar_with_pos_tags(base_grammar, tagged_s)
+    parser = ChartParser(grammar)
+    for tree in parser.parse(word_tokenize(sentence)):
+        print(tree)
+        fig, ax = plt.subplots(figsize=(12, 6))
+        tree.draw()
+        plt.show()
+        break
---
+++ natural.py
@@ -0,0 +1,91 @@
+from predlog.engine.grouper import Grouper
+from typing import List
+from warnings import warn
+
+try:
+    from pymorphy2 import MorphAnalyzer
+    from razdel import tokenize
+
+    morph = MorphAnalyzer()
+
+except ModuleNotFoundError:
+    can_install = input("Russian language is dependent on 3-parties libs: pymorphy2, razdel. Do you want me to install them? (yes/no)")
+    if can_install.lower() == "yes":
+        import subprocess
+        import sys
+        
+        subprocess.check_call([sys.executable, "-m", "pip", "install", 'pymorphy2'])
+        subprocess.check_call([sys.executable, "-m", "pip", "install", 'razdel'])
+        
+        from pymorphy2 import MorphAnalyzer
+        from razdel import tokenize
+
+        morph = MorphAnalyzer()
+    else:
+        warn("Russian language parser is disabled, install pymorphy2, razdel and restart the program")
+
+
+
+simple_sub_conjs = ['который', 'что', 'где', 'если', 'чтобы', 'как', 'ибо', 'пока', 'словно']
+complex_sub_conjs = ['потому что', 'с тех пор как', 'так что', 'лишь только',
+                      'оттого что', 'как будто']
+
+russian_grammar = """
+<CONJ> ::= ,? CONJ | ; | ,
+
+// причастный и деепричастный обороты
+<PART> ::= <CONJ> #* PRTF #+ (<CONJ>|PNCT)| <PART> <CONJ> <PART>
+<TRANS> ::= <CONJ> #* GRND #+ (<CONJ>|PNCT) | <TRANS> <CONJ> <TRANS>
+
+// объект субъект и глагол
+<OBJ> ::= (PRTF|NUMR|ADJF)* (NOUN|NPRO) | PREP (nomn_NOUN|nomn_NPRO|<OBJ>)
+<SUBJECT> ::= (PRTF|NUMR|ADJF)* (nomn_NOUN|nomn_NPRO) | <SUBJECT> <CONJ> <SUBJECT>
+<VERB> ::= (VERB|ADJS|COMP|PRTS|INFN|GRND|PRED|PRCL)+
+
+// именное наречное и глагольное словосочетания
+<NP> ::= <OBJ>+ (INFN|ADVB)? | <NP> <CONJ> <NP>
+<AP> ::= PRCL? ADVB (ADVB|<OBJ>|ADJF)* | <AP> <CONJ> <AP>
+<VP> ::=  <VERB> (<AP>|<NP>)? | <VP> <CONJ> <VP>
+
+// подчинительное предложение и прямая речь
+<SUB_SENT> ::= <CONJ> #* SUB_CONJ #+ <CONJ> | <SUB_SENT> (<CONJ>|SUB_CONJ) <SUB_SENT>
+<DS> ::= : « #+ »
+
+// вопросительное предложение
+<QUESTION> ::= #+ Q
+// восклицательное предложение
+<EXCL> ::= #+ !
+// утвердительное предложение
+<AFFIRM> ::= (<SUBJECT>|PRCL|<NP>|<AP>|<VP>|<TRANS>|<PART>|<SUB_SENT>|<DS>)+ .?
+// утвердительные предложения мы можем связывать в цепочку
+<SEN> ::= <AFFIRM> (<CONJ> <AFFIRM>)* 
+"""
+
+def preprocessing(token: str) -> str:
+    if token in [',', ';', ':', '«', '»', '!', '.']:
+        return token
+    if token == '?':
+      return 'Q'
+    if ' '.join(token.split('_')) in complex_sub_conjs+simple_sub_conjs:
+        return 'SUB_CONJ'  # subordinate conjunction
+    tag = morph.parse(token)[0].tag
+    pos = tag.POS
+    if pos in ['NOUN', 'NPRO'] and 'nomn' in tag.case:
+        return f'{tag.case}_{pos}'
+    if pos is not None:
+        return pos
+    elif 'PNCT' in tag:
+        return 'PNCT'
+    else:
+        return token
+
+def tokenizer(text: str) -> List[str]:
+    for complex_sub_conj in complex_sub_conjs:
+        if complex_sub_conj in text:
+            text = text.replace(complex_sub_conj, '_'.join(complex_sub_conj.split()))
+    return [t.text for t in tokenize(text)]
+
+
+russian_parser = Grouper(rules=russian_grammar,
+                 tokenizer=tokenizer,
+                 mirror=preprocessing)
